eec89212c7ca2be74e44fb07a83c81653c598402
AlphaSteam <sebastian.alfaro@ing.uchile.cl>
2023-11-19 07:20:34 -0300

    Experimental change

--- tasks/publish.sh
+++ tasks/publish.sh
@@ -26,10 +26,10 @@ set -x
 cd ..
 root_path=$PWD
 
-if [ -n "$(git status --porcelain)" ]; then
-  echo "Your git status is not clean. Aborting.";
-  exit 1;
-fi
+# if [ -n "$(git status --porcelain)" ]; then
+#   echo "Your git status is not clean. Aborting.";
+#   exit 1;
+# fi
 
 # Compile
 npm run build:prod -w react-error-overlay
